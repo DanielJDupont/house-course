5c8b743075f7cc6aac068c0df33296aba323907f
DanielJDupont <ddupont2@uwo.ca>
2021-02-26 14:54:07 -0500

    2nd law of thermodynamics

--- pages/houses/add.tsx
+++ pages/houses/add.tsx
@@ -1,8 +1,22 @@
-// import { GetServerSideProps, NextApiRequest } from "next";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
-// import Layout from "src/components/layout";
-// import HouseForm from "src/components/houseForm";
+import { GetServerSideProps, NextApiRequest } from "next";
+import { loadIdToken } from "src/auth/firebaseAdmin";
+import Layout from "src/components/layout";
+import HouseForm from "src/components/houseForm";
 
 export default function Add() {
-  return <div>Add</div>;
+  return <Layout main={<HouseForm />} />;
 }
+
+// GetServerSideProps just means that all functions in here automatically have a req and res prop.
+// Now this is in pages... not components. I think all pages have access to server side props.
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const uid = await loadIdToken(req as NextApiRequest);
+
+  if (!uid) {
+    res.setHeader("location", "/auth");
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return { props: {} };
+};
--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,5 +1,5 @@
-// import { useState, useEffect, ChangeEvent } from "react";
-// import { useForm } from "react-hook-form";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
@@ -15,4 +15,71 @@
 // } from "src/generated/UpdateHouseMutation";
 // import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
 
-export {};
+interface IFormData {
+  address: string;
+  latitude: number;
+  longitude: number;
+  bedrooms: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function HouseForm({}: IProps) {
+  const [submitting, setSubmitting] = useState(false);
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
+
+  useEffect(() => {
+    register({ name: "address" }, { required: "Please enter your address" });
+    register({ name: "latitude" }, { required: true, min: -90, max: 90 });
+    register({ name: "longitude" }, { required: true, min: -180, max: 180 });
+  }, [register]);
+
+  // Note that handleSubmit does not know what to even do with the submission, we must instruct it how.
+  const onSubmit = (data: IFormData) => {
+    setSubmitting(true);
+    // Creation of a house.
+    handleCreate(data);
+  };
+
+  // onSubmit means it is successful, you cna use an onError for the second argument of handleSubmit to manage an error.
+
+  const handleCreate = async (data: IFormData) => {};
+
+  return (
+    // We need to pass in a function into handleSubmit so handleSubmit actually knows what to even do with the form it is given with the onClick.
+    <form className="mx-auto max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add a New House</h1>
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for your address
+        </label>
+        {/* SEARCH FIELD */}
+        {errors.address && <p>{errors.address.message}</p>}
+      </div>
+    </form>
+  );
+}
+
+/*
+Note that you can use typescript check with: yarn tsc, to be certain that VSCode isn't missing any TypeScript error messages.
+
+What is register with react hook form?
+https://react-hook-form.com/api#register
+
+required, boolean, if a value is required before submission.
+maxLength: max length of value.
+minLength
+max, maximum value.
+min
+pattern, a regex that must be present on the input value.
+
+validate, can pass either one callback function or many callback functions to ensure that your input is valid.
+To pass many callback functions is odd, rather than a list of callback functions the author decided to make it fields.
+You can use the field keys to name each callback function meaningfully, whereas a list would not be able to do this.
+
+valueAsDate, text input only, returns a Date string.
+setValueAs, return input value by running through the function.
+*/
